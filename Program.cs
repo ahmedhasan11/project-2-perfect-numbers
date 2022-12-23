fefeff8fdab07ce919d545433ca301d528575c70
ahmedhasan11 <121199845+ahmedhasan11@users.noreply.github.com>
2022-12-23 18:15:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2_perfect_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, n1, n2, i, j;
+            Console.WriteLine("please enter n1");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter n2");
+            n2 = int.Parse(Console.ReadLine());
+            for (i = n1; i <= n2; i++)
+            {
+                for (j = 1; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                        sum += j;
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine("the perfect number is" + " " + i);
+                    sum = 0;
+                }
+                else
+                {
+                    sum = 0;
+                }
+            }
+        }
+    }
+}
